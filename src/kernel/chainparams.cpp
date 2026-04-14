53e930657b198870f3140298127b6025ada9b884
Bitwebisss <196428942+Bitwebisss@users.noreply.github.com>
2026-04-14 21:32:24 +0300

    11

--- src/kernel/chainparams.cpp
+++ src/kernel/chainparams.cpp
@@ -264,7 +264,7 @@ public:
         // Checkpoints restored
         checkpointData = {
             {
-                {0, uint256{"d57b558193dd042caa90f3b3b06626cecd47fd0d09ecb7e355ae21f8f64b47b1"}},
+                {580, uint256{"8b9881e75e1a0850f9dc68147b974dc4f1e974bdd6cd15a3cc242239b060599c"}},
             }
         };
         // Checkpoints restored
