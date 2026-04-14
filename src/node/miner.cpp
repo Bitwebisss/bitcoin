56582b17acdfdb1e1a2d42c6de97b94d20b780cf
Bitwebisss <196428942+Bitwebisss@users.noreply.github.com>
2026-04-14 11:18:06 +0300

    Update miner.cpp

--- src/node/miner.cpp
+++ src/node/miner.cpp
@@ -48,6 +48,13 @@ int64_t UpdateTime(CBlockHeader* pblock, const Consensus::Params& consensusParam
         pblock->nTime = nNewTime;
     }
 
+    // Updating time can change work required on testnet:
+    /*
+    if (consensusParams.fPowAllowMinDifficultyBlocks) {
+        pblock->nBits = GetNextWorkRequired(pindexPrev, pblock, consensusParams);
+    }
+    */    
+
     return nNewTime - nOldTime;
 }
 
@@ -465,6 +472,7 @@ std::unique_ptr<CBlockTemplate> WaitAndCreateNewBlock(ChainstateManager& chainma
     auto now{NodeClock::now()};
     const auto deadline = now + options.timeout;
     const MillisecondsDouble tick{1000};
+    // const bool allow_min_difficulty{chainman.GetParams().GetConsensus().fPowAllowMinDifficultyBlocks};
 
     do {
         bool tip_changed{false};
@@ -492,6 +500,16 @@ std::unique_ptr<CBlockTemplate> WaitAndCreateNewBlock(ChainstateManager& chainma
 
         // Must release m_tip_block_mutex before locking cs_main, to avoid deadlocks.
         LOCK(::cs_main);
+
+        // On test networks return a minimum difficulty block after 20 minutes
+		/*
+        if (!tip_changed && allow_min_difficulty) {
+            const NodeClock::time_point tip_time{std::chrono::seconds{chainman.ActiveChain().Tip()->GetBlockTime()}};
+            if (now > tip_time + 20min) {
+                tip_changed = true;
+            }
+        }
+        */
 
         /**
          * We determine if fees increased compared to the previous template by generating
