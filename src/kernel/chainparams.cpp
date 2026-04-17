7ff153f267542233627150d1218774c7f06ac7c1
Bitwebisss <196428942+Bitwebisss@users.noreply.github.com>
2026-04-17 22:59:56 +0300

    Update chainparams.cpp

--- src/kernel/chainparams.cpp
+++ src/kernel/chainparams.cpp
@@ -93,7 +93,7 @@ public:
         consensus.script_flag_exceptions.emplace( // Taproot exception
             uint256{"0000000000000000000f14c35b2d841e986ab5441de8c585d5ffe55ea1e395ad"}, SCRIPT_VERIFY_P2SH | SCRIPT_VERIFY_WITNESS);
         */
-        consensus.BIP34Height = 1; // Active from the start
+        consensus.BIP34Height = 112; // Active from the start
         consensus.BIP34Hash = uint256{}; 
         consensus.BIP65Height = 1; // Active from the start
         consensus.BIP66Height = 1; // Active from the start
