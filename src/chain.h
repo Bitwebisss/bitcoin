5aa345b238be559ad795f68c5aac64182acf5cfb
Bitwebisss <196428942+Bitwebisss@users.noreply.github.com>
2026-04-18 17:21:26 +0300

    Update chain.h

--- src/chain.h
+++ src/chain.h
@@ -26,7 +26,7 @@
  * Maximum amount of time that a block timestamp is allowed to exceed the
  * current time before the block will be accepted.
  */
-static constexpr int64_t MAX_FUTURE_BLOCK_TIME = 60 * 60 * 24; /* 2 Blocks Time Wrape Protect - Bitweb Params */
+static constexpr int64_t MAX_FUTURE_BLOCK_TIME = 2 * 60 * 60; /* 2 Blocks Time Wrape Protect - Bitweb Params */
 
 /**
  * Timestamp window used as a grace period by code that compares external
