d8b4f41ebd9064aede8a8a4842ffc8ed8ee1bf6e
Bitwebisss <196428942+Bitwebisss@users.noreply.github.com>
2026-04-19 05:05:46 +0300

    Update feature_pruning.py

--- test/functional/feature_pruning.py
+++ test/functional/feature_pruning.py
@@ -11,6 +11,7 @@ This test takes 30 mins or more (up to 2 hours)
 import os
 
 from test_framework.blocktools import (
+    TIMESTAMP_WINDOW,
     MIN_BLOCKS_TO_KEEP,
     create_block,
     create_coinbase,
@@ -31,7 +32,6 @@ from test_framework.util import (
 # Rescans start at the earliest block up to 2 hours before a key timestamp, so
 # the manual prune RPC avoids pruning blocks in the same window to be
 # compatible with pruning based on key creation time.
-TIMESTAMP_WINDOW = 2 * 60 * 60
 
 def mine_large_blocks(node, n):
     # Make a large scriptPubKey for the coinbase transaction. This is OP_RETURN
