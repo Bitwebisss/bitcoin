a952626b06661e0c57bd2029e058c0a9faed0f4c
Bitwebisss <196428942+Bitwebisss@users.noreply.github.com>
2026-04-24 17:36:32 +0300

    12222333444

--- ci/test/03_test_script.sh
+++ ci/test/03_test_script.sh
@@ -165,14 +165,6 @@ if [ "$RUN_CHECK_DEPS" = "true" ]; then
   "${BASE_ROOT_DIR}/contrib/devtools/check-deps.sh" "${BASE_BUILD_DIR}"
 fi
 
-if [[ "$CI_OS_NAME" == "macos" && "${GOAL}" = "install deploy" ]]; then
-  unzip "${BASE_BUILD_DIR}/Bitweb-Core.zip" -d "${BASE_BUILD_DIR}/deploy"
-  if ! ( codesign --verify "${BASE_BUILD_DIR}/deploy/Bitweb-Qt.app" ); then
-    echo "Codesigning failed."
-    false
-  fi
-fi
-
 if [ "$RUN_UNIT_TESTS" = "true" ]; then
   DIR_UNIT_TEST_DATA="${DIR_UNIT_TEST_DATA}" \
   LD_LIBRARY_PATH="${DEPENDS_DIR}/${HOST}/lib" \
