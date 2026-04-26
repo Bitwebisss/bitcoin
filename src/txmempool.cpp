777e83e760f227b22c26fc10833400f0f77559d8
Bitwebisss <196428942+Bitwebisss@users.noreply.github.com>
2026-04-26 05:55:58 +0300

    Update txmempool.cpp

--- src/txmempool.cpp
+++ src/txmempool.cpp
@@ -9,6 +9,7 @@
 #include <coins.h>
 #include <common/system.h>
 #include <consensus/consensus.h>
+#include <chainparams.h>  // Bitweb Params: remove or comment when logic removed
 #include <consensus/tx_verify.h>
 #include <consensus/validation.h>
 #include <logging.h>
