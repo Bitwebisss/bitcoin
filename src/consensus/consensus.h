6ec61ebd2488a64e29327034ab88fa672dde5009
mraksoll4 <32709596+mraksoll4@users.noreply.github.com>
2026-05-25 22:37:40 +0300

    Update

--- src/consensus/consensus.h
+++ src/consensus/consensus.h
@@ -19,7 +19,7 @@ static const int64_t MAX_BLOCK_SIGOPS_COST = 80000;
 static const int COINBASE_MATURITY = 100;
 
 //Remove me after ext period end.
-static const int EXT_COINBASE_MATURITY = 9000;  // Bitweb Params
+static const int EXT_COINBASE_MATURITY = 300;  // Bitweb Params
 
 static const int WITNESS_SCALE_FACTOR = 4;
 
